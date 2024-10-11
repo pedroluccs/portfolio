caf295b4e1eb491afd093fe7374e30e0d176fea9
Pedro Lucas <pedro.wo@live.com>
2024-10-11 18:13:21 -0400

    alteracao section sobre mim

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -6,12 +6,12 @@ const Sobre = () => (
   <section>
     <Titulo fontSize={16}>Sobre mim</Titulo>
     <Paragrafo>
-      Sou desenvolvedor front-end com experiência em HTML, CSS (SASS e LESS),
-      JavaScript, TypeScript e frameworks como React, utilizo boas práticas de
-      desenvolvimento como a metodologia BEM e Styled Components para construir
-      aplicações escaláveis e eficientes. Também faço uso de ferramentas como
-      Redux Toolkit para gerenciamento de estado global e otimização da
-      performance.
+      Sou desenvolvedor front-end com experiência em HTML, CSS (SASS e LESS) e
+      JavaScript, incluindo TypeScript e frameworks como React. Sigo as melhores
+      práticas de desenvolvimento, como a metodologia BEM e Styled Components,
+      para criar aplicações escaláveis e eficientes. Além disso, utilizo
+      ferramentas como Redux Toolkit para gerenciamento global de estado e
+      otimização de performance.
     </Paragrafo>
     <GithubSecao>
       <img src="https://github-readme-stats.vercel.app/api?username=pedroluccs&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
